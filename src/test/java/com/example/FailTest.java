87de9743774a456a120490c026b163e5b7af964e
Anand Shekhar <anandshekhar1000@gmail.com>
2026-01-17 12:02:49 +0530

    Added failing unit test

---
+++ src/test/java/com/example/FailTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FailTest {
+
+    @Test
+    public void testFailure() {
+        assertTrue(false);   // Intentional failure
+    }
+}
